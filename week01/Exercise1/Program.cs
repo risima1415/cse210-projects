a98011c0156d9159635bb0d073552c8d3694ea34
risima1415 <risima.t.maluleke15@gmail.com>
2026-03-10 12:40:54 +0200

    Added input of name and surname

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string surname = Console.ReadLine();
+        Console.WriteLine($"Your name is {surname}, {name} {surname}.");
     }
 }
